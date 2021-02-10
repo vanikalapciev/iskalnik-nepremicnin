81b115e7403fc843d8e656fc2027384051f7237e
vanikalapciev <31218797+vanikalapciev@users.noreply.github.com>
2021-02-10 07:53:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from exporter import *
 from scraper import parse_pages_napremicnine, pase_pages_bolha
 
 
-#link = sys.argv[1]
-link = "https://www.bolha.com/prodaja-stanovanja/osrednjeslovenska?buildingState%5Bnew-building%5D=1"
+link = sys.argv[1]
 
 if "nepremicnine.net" in link:
 	main_list=parse_pages_napremicnine(link)
